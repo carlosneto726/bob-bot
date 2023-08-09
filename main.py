0135867344a6aca808cc325a3052a50513e5a9ef
Juliano Marcelo Schaurich Júnior <schaurich.jmsj@gmail.com>
2023-08-09 17:06:00 -0300

    member

--- main.py
+++ main.py
@@ -5,15 +5,23 @@ import discord, os
 load_dotenv(find_dotenv())
 
 class MyClient(discord.Client):
-    async def on_ready(self):
-        print(f'Logged on as {self.user}!')
+  async def on_ready(self):
+    print(f'Logged on as {self.user}!')
 
-    async def on_message(self, message):
-        if message.author == client.user:
-          return
+  async def on_message(self, message):
+    if message.author == client.user:
+      return
         
-        if message.content.lower() == "f":
-          await message.channel.send("F")
+    if message.content.lower() == "f":
+      await message.channel.send("F")
+
+  async def on_member_join(member):
+    channel = client.get_channel("1033440648843501570")
+    await channel.send(f"{member} has joined the server")
+
+  async def on_member_remove(member):
+    channel = client.get_channel("1033440648843501570")
+    await channel.send(f"{member} has left the server")
 
 intents = discord.Intents.default()
 intents.members = True
